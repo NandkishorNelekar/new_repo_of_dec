ce8c4e0e72d9850582b9f4535a84898b30637863
NandkishorNelekar <nelekarnandkishor@gmail.com>
2026-01-01 13:31:37 +0530

    file is commited

--- checknumber.java
+++ checknumber.java
@@ -1,11 +1,11 @@
 public class checknumber{
 	public static void main(String[] args){
 		int a = 5;
-		if(a%2==0){
-			System.out.println("number is even");
+		if(a>=0){
+			System.out.println("number is positive");
 		}
 else{
-	System.out.println("number is odd");
+	System.out.println("number is negative");
 
 }
 	}
